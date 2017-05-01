e3c9203e16878f83055aadbbb2b2219672eff5f2
New-bottle <775712558@qq.com>
2017-05-01 10:53:54 +0800

    server.cpp writing

--- train-ticket-system-server/server.cpp
+++ train-ticket-system-server/server.cpp
@@ -1,3 +1,34 @@
 #include "server.h"
+#include "account_manager.h"
 
-Server::Register
+void Server::Register(const std::wstring& ID, const std::wstring& password, const std::wstring& name)
+{
+    //if insert failed
+    if (!users.insert(pair(ID, (User)(ID, password, name))).second)
+        throw Have_registered();
+}
+
+Account* Server::login(const std::__cxx11::wstring &ID, const std::__cxx11::wstring &password)
+{
+    Account** ans;
+    auto iter = users.find(ID);
+    if (iter != users.end()) {
+        ans = &(iter -> second);
+    }
+    return ans;
+}
+
+void Server::import_accounts(ifstream &fin)
+{
+
+}
+
+void Server::import_options(ifstream &fin)
+{
+
+}
+
+void Server::import_trains(ifstream &fin)
+{
+
+}
