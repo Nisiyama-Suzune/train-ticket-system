8a4979b1f22999d47b48a53213a0467096833842
New-bottle <775712558@qq.com>
2017-05-13 19:50:51 +0800

    update several ui

--- UI/userbuy.cpp
+++ UI/userbuy.cpp
@@ -26,3 +26,15 @@ void userbuy::on_pushButton_2_clicked()
     userticket u_tic;
     u_tic.show();
 }
+
+void userbuy::on_pushButton_6_clicked()
+{
+
+}
+
+void userbuy::on_query_with_station_clicked()
+{
+	QString start_station = ui->start_station->currentText(),
+			end_station = ui->end_station->currentText();
+	ui->train_list->
+}
