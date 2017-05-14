13a1594cf084bb06627ecc88cdef9996e201874b
SupremeT <1012143610@qq.com>
2017-05-14 10:57:34 +0800

    add to query.h

--- tts_server/header/query.h
+++ tts_server/header/query.h
@@ -5,12 +5,39 @@
 
 struct query_ticket_data {
     QString dep_city;
-    //....
+
 };
 
 struct query_ticket_ans {
     QString train_name;
-    //...
+    int start_date;
+    QString start_station;
+    QString start_time;
+    QString end_station;
+    QString end_time;
+    QString seat_kind;
+    int ticket_left;
 };
+
+struct query_my_order
+{
+    QString train_name;
+    int start_date;
+    QString start_station;
+    QString start_time;
+    QString end_station;
+    QString end_time;
+    QString seat_kind;
+    int ticket_number;
+};
+
+/*函数接口名
+ *
+ * query_city_city
+ * query_station_station
+ * add_line
+ * delete_line
+ * */
+
 
 #endif // QUERY_H
