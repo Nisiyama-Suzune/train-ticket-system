f983df0c4bdad8884a079699ebad8955edf8681a
AaronARen <ryw_pub@126.com>
2017-05-14 10:45:03 +0800

    query.h,query_ticket

---
+++ tts_server/header/query.h
@@ -0,0 +1,16 @@
+#ifndef QUERY_H
+#define QUERY_H
+
+#include <QString>
+
+struct query_ticket_data {
+    QString dep_city;
+    //....
+};
+
+struct query_ticket_ans {
+    QString train_name;
+    //...
+};
+
+#endif // QUERY_H
